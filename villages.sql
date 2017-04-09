ea0f2fda67d9988fdf1aebe5a61426c3bc95561f
Vivek Singh <petmongrels@gmail.com>
2017-04-09 22:06:55 +0530

    remove sql based form creation with json based creation

---
+++ villages.sql
@@ -0,0 +1,56 @@
+DELETE from address_level;
+
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Ghotpadi', 1, 'ccc938bf-7c53-4e41-836c-3ee65db09b9c', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Bodonge', 1, '8dcc0782-a5c5-4f93-a1a0-456a4931949c', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Bhatpar', 1, '6abfcf28-4fa4-4cc8-a07c-addf983b0e9d', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Darbha', 1, '76925c1a-c845-4d67-8807-36184178dca7', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Juwi', 1, 'da5d90e1-3ff6-4f78-ad34-f7002bb9bf08', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Jinjagaon', 1, '9ab9abb2-f286-4b28-aaa9-ab342ade3d55', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Maraampalli', 1, '27ff8867-f6dc-47c8-ae0a-88fa6b306746', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Gurnur', 1, 'd71e0f75-aaf7-4bca-8223-889e4a9be0d9', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Chichoda', 1, '676b1e3d-c828-4453-92cd-934e629e02e5', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Rela', 1, '61757ad1-cbab-42d0-a65d-36998ec044b5', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Pengunda', 1, '06adbb4e-2341-453c-b631-1a8636358d91', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Kucher', 1, 'f3af68f3-4b91-4f95-8123-23cfdf7f8714', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Mahakapadi', 1, '37bedc4f-1144-4cbf-961d-0c17285672ca', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Boriya', 1, 'aa565a6d-7c05-4676-bc6b-fc306a973962', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Gopnar', 1, 'c7739a4c-0af7-48eb-871b-f86cde880f62', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Hodri', 1, '067bdce6-cdc8-4e98-bc1e-7753dcae7b32', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Lashkar', 1, 'fc256988-e2d8-4faa-831b-60476c769f8c', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Aaldandi', 1, 'c454ef3f-8197-4a56-95f6-92fba03615a9', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Moradpur', 1, 'e7dbc783-4014-42b9-9fd1-32b1a8665c45', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Parenar', 1, '9d3b3fc5-781e-4016-bab6-eef4a0533fd1', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Midapalli', 1, '8b5282c5-cf10-4c1b-af05-c4cb99ea1cbf', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Nilgunda', 1, '50545663-327d-49d8-ae28-181691279e72', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Kavande', 1, '4080b488-4f3f-4c0a-b749-c68ae78dd60d', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Morometta', 1, 'f47870ea-3f50-4065-96c2-b70a09027b10', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Mardmallenga', 1, 'bb004171-dcf7-4731-883f-57bb6008c9bb', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Ittalvar', 1, '45ae266c-3aba-4c18-bee4-c797e8ad9d78', 1, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Other Village', 1, 'f8e98809-1397-4635-b5bc-9ba51767fa0e', 1, 1, 1, current_timestamp, current_timestamp);
