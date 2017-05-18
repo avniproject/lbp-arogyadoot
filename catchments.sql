0f7571df03130e603e3f6dc08cfbbd33cb65aaa8
Arjun <arjunk@thoughtworks.com>
2017-05-18 13:00:02 +0530

    Adding catchment

---
+++ catchments.sql
@@ -0,0 +1,34 @@
+DELETE from catchment_address_mapping;
+DELETE from catchment;
+
+insert into catchment (name, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('first catchment','48adfc10-c501-48ab-87b6-622b48e64682', 1, 1, 1, current_timestamp, current_timestamp);
+
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (1, 1, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (1, 2, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (1, 3, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (1, 4, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (1, 5, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (1, 6, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (1, 7, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (1, 8, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (1, 9, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (1, 10, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (1, 11, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (1, 12, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (1, 13, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (1, 14, 1, 1, 1, current_timestamp, current_timestamp);
