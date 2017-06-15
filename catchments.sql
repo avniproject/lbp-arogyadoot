0f28eb879924d468d5168150e04e526f69c9e6e2
Vivek Singh <petmongrels@gmail.com>
2017-06-15 16:21:38 +0530

    two catchments fixed typo

--- catchments.sql
+++ catchments.sql
@@ -32,3 +32,27 @@ insert into catchment_address_mapping (catchment_id, addresslevel_id, version, c
 VALUES (1, 13, 1, 1, 1, current_timestamp, current_timestamp);
 insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
 VALUES (1, 14, 1, 1, 1, current_timestamp, current_timestamp);
+
+
+
+insert into catchment (name, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('second catchment','f7afc38d-3531-44b9-823f-2569f20e5ff1', 1, 1, 1, current_timestamp, current_timestamp);
+
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (2, 15, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (2, 16, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (2, 17, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (2, 18, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (2, 19, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (2, 19, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (2, 20, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (2, 21, 1, 1, 1, current_timestamp, current_timestamp);
+insert into catchment_address_mapping (catchment_id, addresslevel_id, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (2, 22, 1, 1, 1, current_timestamp, current_timestamp);
