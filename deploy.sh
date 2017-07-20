c0e64ad36373297642c8c5c2b6b91ce2600f016c
Arjun <arjunk@thoughtworks.com>
2017-07-20 11:17:20 +0530

    Changes related to deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 echo "executing the impl setup"
 psql -h $OPENCHS_DATABASE_HOST -U openchs -q -f $IMPL_DIR/villages.sql
 psql -h $OPENCHS_DATABASE_HOST -U openchs -q -f $IMPL_DIR/catchments.sql
-curl -X POST http://$(OPENCHS_SERVER):$(OPENCHS_SERVER_PORT)/forms -d @$IMPL_DIR/registrationForm.json -H "Content-Type: application/json"
+curl -X POST http://$OPENCHS_SERVER:$OPENCHS_SERVER_PORT/forms -d @$IMPL_DIR/registrationForm.json -H "Content-Type: application/json"
