1dde64b46a5df116851a7e0dc9d4107d50a485e4
mihirk <mihir.khatwani@gmail.com>
2018-03-28 12:38:34 +0530

    Remove unused file

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-echo "executing the impl setup"
-curl -X POST http://$OPENCHS_SERVER:$OPENCHS_SERVER_PORT/catchments -d @./lbp/catchments.json -H "Content-Type: application/json"
-curl -X POST http://$OPENCHS_SERVER:$OPENCHS_SERVER_PORT/forms -d @$IMPL_DIR/registrationForm.json -H "Content-Type: application/json"
