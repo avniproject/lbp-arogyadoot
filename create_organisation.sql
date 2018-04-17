a0a062e81d5da98a23f7fb554f665697dbc2e56d
Charles John <charlesj@gmail.com>
2018-04-17 09:57:09 +0530

    db_user name corrections

--- create_organisation.sql
+++ create_organisation.sql
@@ -1,11 +1,11 @@
 
 CREATE ROLE lokbiradari_prakalp NOINHERIT PASSWORD 'password';
 
-GRANT lbp TO openchs;
+GRANT lokbiradari_prakalp TO openchs;
 
-GRANT ALL ON ALL TABLES IN SCHEMA public TO lbp;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO lbp;
-GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO lbp;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO lokbiradari_prakalp;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO lokbiradari_prakalp;
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO lokbiradari_prakalp;
 
 INSERT into organisation(name, db_user, uuid, parent_organisation_id)
     SELECT 'Lokbiradari Prakalp', 'lokbiradari_prakalp', '3de82517-7a60-4b83-b98a-53d9c89eba02', id FROM organisation WHERE name = 'OpenCHS';
