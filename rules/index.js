01b066cf2fc105256c9f5aad454008f4f03e57e7
mihirk <mihir.khatwani@gmail.com>
2018-06-19 16:05:27 +0530

    Adjusting to new contract for rules config

--- rules/index.js
+++ rules/index.js
@@ -1,9 +1,9 @@
-const RuleFactory = require('rules-config/config');
+const {RuleFactory} = require('rules-config/rules');
 
 
 const ANCFormProgramEncounterDecisions = RuleFactory("3a95e9b0-731a-4714-ae7c-10e1d03cebfe", "decision");
 
-@ANCFormProgramEncounterDecisions("d35e3039-eeb7-4c1b-b02f-88f492163500", "Calcium 1g/Day", {})
+@ANCFormProgramEncounterDecisions("d35e3039-eeb7-4c1b-b02f-88f492163500", "Calcium 1g/Day", {ruleData: "more"})
 class Calcium1gDay {
     static exec(programEncounter, decisions, today) {
         return {"name": "Treatment", "value": "e4ccf02c-9767-4641-ab76-5128e2a1f0d9"};
