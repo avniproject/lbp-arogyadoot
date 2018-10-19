34a4bd60bb99111ef64b6a6ccbcb67d0dd8a028c
Abhishek <abhiverma.abhishek@gmail.com>
2018-10-19 16:26:38 +0530

    removed rule for stool related complaints from lbp and moved to core

--- rules/rules.js
+++ rules/rules.js
@@ -1,7 +1,6 @@
 const {RuleFactory, FormElementStatusBuilder, FormElementsStatusHelper} = require('rules-config/rules');
 
 const RegistrationFormHandler = RuleFactory("881f0ddb-ce35-4372-abae-622fb04bc236", "ViewFilter");
-const ChildPNCFormHandler = RuleFactory("e09dddeb-ed72-40c4-ae8d-112d8893f18b","ViewFilter");
 
 @RegistrationFormHandler("f9d02ea8-af32-4e08-8564-2023a532cccf", "[LBP] Registration View Form Handler", 1.0, {})
 class RegistrationFormHandlerLBP {
@@ -29,19 +28,5 @@ class RegistrationFormHandlerLBP {
     }
 }
 
-@ChildPNCFormHandler("f9d02ea8-af32-4e08-8564-2023a532cccf", "[LBP] Child PNC View Form Handler", 1.0, {})
-class childPNCFormHandlerLBP{
 
-    stoolRelatedComplaints(programEncounter, formElement) {
-        let formElementStatusBuilder = new FormElementStatusBuilder({programEncounter, formElement});
-        formElementStatusBuilder.skipAnswers("Loose stools");
-        return formElementStatusBuilder.build();
-    }
-
-    static exec(programEncounter, formElementGroup, today) {
-        return FormElementsStatusHelper
-            .getFormElementsStatusesWithoutDefaults(new childPNCFormHandlerLBP(), programEncounter, formElementGroup, today);
-    }
-}
-
-module.exports = {RegistrationFormHandlerLBP, childPNCFormHandlerLBP};
+module.exports = {RegistrationFormHandlerLBP};
