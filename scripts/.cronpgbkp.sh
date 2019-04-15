6fd2ca0380d93dd64e93ca50338b77ae7796e7f3
Sidtharthan A N <sidthunagarajan@gmail.com>
2019-04-15 14:08:47 +0530

    0000 | setup db backup cron jobs

--- scripts/.cronpgbkp.sh
+++ scripts/.cronpgbkp.sh
@@ -1,23 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/env bash
 
-PATH=postgres-path:$PATH
 tstamp=$(date -Iseconds)
 log=$HOME/.cronpgbkp/$2.$tstamp.log
 sql=$HOME/.cronpgbkp/$2.$tstamp.sql
 
 mkdir -p $HOME/.cronpgbkp
 
-until pg_isready -h localhost 1> /dev/null 2>> $log
+echo "[$tstamp] Starting" >> $log
+sudo service postgresql-9.6 start 1>>$log 2>&1
+
+until /usr/pgsql-9.6/bin/pg_isready -h localhost 1>>$log 2>&1
 do
-    echo Waiting >> $log
-    sleep 1
+    echo "[$tstamp] Waiting" >> $log
+    sleep 5
 done
 
-echo 'Postgres Service Ready' >> $log
+echo "[$tstamp] Postgres Service Ready" >> $log
 
-pg_dump -U$1 -hlocalhost $2 > $sql 2>> $log
+/usr/pgsql-9.6/bin/pg_dump -U$1 -hlocalhost $2 > $sql 2>> $log
 
-echo 'Backup done' >> $log
+echo "[$tstamp] Backup done" >> $log
 
 # At Reboot
 # @reboot $HOME/.cronpgbkp.sh admin openchs
@@ -25,11 +27,14 @@ echo 'Backup done' >> $log
 #
 # Hourly
 # 0 */1 * * * $HOME/.cronpgbkp.sh admin openchs
-# 0 */1 * * * $HOME/.cronpgbkpdel.sh openchs*.sql 10
-# 0 */1 * * * $HOME/.cronpgbkpdel.sh openchs*.log 10
+# 30 */1 * * * $HOME/.cronpgbkpdel.sh openchs*.sql 10
+# 30 */1 * * * $HOME/.cronpgbkpdel.sh openchs*.log 10
 #
 # Every 3 Hours
 # 0 */3 * * * $HOME/.cronpgbkp.sh admin reportingdb
-# 0 */3 * * * $HOME/.cronpgbkpdel.sh reportingdb*.sql 2
-# 0 */3 * * * $HOME/.cronpgbkpdel.sh reportingdb*.log 2
+# 30 */3 * * * $HOME/.cronpgbkpdel.sh reportingdb*.sql 2
+# 30 */3 * * * $HOME/.cronpgbkpdel.sh reportingdb*.log 2
+#
+# visudo
+# admin ALL=(ALL) NOPASSWD: /home/admin/.cronpgbkp.sh
 #
--- scripts/.cronpgbkpdel.sh
+++ scripts/.cronpgbkpdel.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/env bash
 
 ls -t $HOME/.cronpgbkp/$1 | tail -n+$((1+$2)) | xargs -I {} rm -- {}
